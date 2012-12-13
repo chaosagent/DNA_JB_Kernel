ab91732703e52b9fbc91482e8d8f5b1aff2e9a5b
Dennis Boyce <dsb9938@yahoo.com>
2012-12-12 19:13:58 -0500

    Added SavagedZen Governor.

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -393,6 +393,9 @@ extern struct cpufreq_governor cpufreq_gov_lionheart;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_BADASS)
 extern struct cpufreq_governor cpufreq_gov_badass;
 #define CPUFREQ_DEFAULT_GOVERNOR  (&cpufreq_gov_badass)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SAVAGEDZEN)
+extern struct cpufreq_governor cpufreq_gov_savagedzen;
+#define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_savagedzen)
 #endif
 
 
